e7f3d84d8d900de7b2e58f68d5198583a8c557af
scodeq65 <sodiqagbaraojo@gmail.com>
2024-07-02 14:47:27 +0100

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
+# Install python 3.8.10
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
 # Puppet manifest to install Flask 2.1.0 using pip3
 
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+#Install Werkzeug
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider =>  'pip3'
+  require  => Package['Flask'],
 }

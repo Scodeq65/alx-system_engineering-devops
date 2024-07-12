eaf3770e8e776047c439aeccaeb5241c9e436fec
scodeq65 <sodiqagbaraojo@gmail.com>
2024-07-12 18:23:31 +0100

    configurations_management

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,19 @@
 # Configure an Nginx server using Puppet
 
-Package { 'nginx':
+# Ensure the nginx package is installed
+package { 'nginx':
   ensure  => installed,
 }
 file_line { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me hhtps://wwww.youtube.come/sodiq permanent;',
+  line   => 'rewrite ^/redirect_me htps://wwww.youtube.come/watch?vQH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
-  content  => 'Hello World!',
+  ensure  => file,
+  content => 'Hello World!',
 }
 
 service {'nginx':

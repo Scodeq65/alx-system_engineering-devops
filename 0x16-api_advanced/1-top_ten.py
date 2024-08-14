3f5d0c0eb6a00fc5629fe80381f3f8a1dfd995c2
scodeq65 <sodiqagbaraojo@gmail.com>
2024-08-14 03:13:14 +0100

    task1_updated

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,18 +2,20 @@
 """ Print the titles of the first 10 hot posts for a given subreddit. """
 import requests
 
+
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed for a given subreddit.
-    
+
+    """Prints the titles of the first 10 posts listed.
+
     Args:
         subreddit (str): The name of the subreddit to query.
-    
+
     Returns:
-        None: Prints the titles of the posts or None if the subreddit is invalid.
+        None: Prints the titles of the posts or None if invalid.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:

b729d22f1d48d55b89cf093f30960b6999eaaec5
scodeq65 <sodiqagbaraojo@gmail.com>
2024-08-16 02:19:33 +0000

    puppet

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Puppet code that correct a wordpress site 500 error to 200 Ok
+# by editing the mistake line of code in /var/www/html/wp-settings.php
+
+exec { 'fix-wp-server-error':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/bin/:bin/',
+}

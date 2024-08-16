13d9a9e4a52bc6e858553581e5d87a2ae09a9c83
scodeq65 <sodiqagbaraojo@gmail.com>
2024-08-16 03:33:51 +0100

    minor_correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,7 @@
 # by editing the mistake line of code in /var/www/html/wp-settings.php
 
 exec { 'fix-wp-server-error':
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path    => '/usr/bin/:bin/',
+    command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+    path    => ['/usr/bin', '/bin/'],
+    onlyif  => 'grep -q "phpp" /var/www/html/wp-settings.php',
 }

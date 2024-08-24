c0911fd040030ce89336a0209075c0a8e671abc3
scodeq65 <sodiqagbaraojo@gmail.com>
2024-08-24 04:47:07 +0100

    task1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Increasing the limit of hard and soft of the /etc/security/limits.conf
+
+#Increase limit
+exec { 'Increase-hard-limit':
+    command => 'sed -i "/holberton hard/s/5/50000" /etc/security/limits.conf',
+    path    => '/bin/',
+}
+
+#Increase soft limit
+exec { 'Increase-soft-limit':
+    command => 'sed -i "/holberton soft/s/4/50000" /etc/security/limits.conf',
+    path    => '/bin/',
+}

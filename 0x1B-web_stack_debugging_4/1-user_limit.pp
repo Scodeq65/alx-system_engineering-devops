383527aed5858e84f69b7df60f7563018b5173fb
scodeq65 <sodiqagbaraojo@gmail.com>
2024-08-24 04:57:11 +0100

    corrected

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,12 @@
 
 #Increase limit
 exec { 'Increase-hard-limit':
-    command => 'sed -i "/holberton hard/s/5/50000" /etc/security/limits.conf',
+    command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
     path    => '/usr/bin/:/bin/',
 }
 
 #Increase soft limit
 exec { 'Increase-soft-limit':
-    command => 'sed -i "/holberton soft/s/4/50000" /etc/security/limits.conf',
+    command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
     path    => '/usr/bin/:/bin/',
 }
